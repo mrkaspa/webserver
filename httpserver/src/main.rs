5f7cae04997ec821391a7b41a47fa1db3af6197a
Michel Perez <michel.ingesoft@gmail.com>
2022-03-04 11:09:33 -0500

    wip

---
+++ httpserver/src/main.rs
@@ -0,0 +1,11 @@
+mod handler;
+mod router;
+mod server;
+use server::Server;
+
+fn main() {
+    // Start a server
+    let server = Server::new("localhost:3000");
+    //Run the server
+    server.run();
+}
